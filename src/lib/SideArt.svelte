60bddad22e8f7b80203706afcf3cbac6d05d95ce
Jonas Stettner <mail@jstet.net>
2022-10-05 01:09:22 +0200

    started creating own generative art

--- src/lib/SideArt.svelte
+++ src/lib/SideArt.svelte
@@ -1,69 +1,66 @@
 <script>
     import { dims, window_dims } from "$lib/stores/dimensions.js";
-    import trianglify from "trianglify";
-    import { onMount } from "svelte";
+    import * as d3 from "d3";
 
     let pattern_container;
 
-    function draw_pattern() {
-        // apply trianglify to convert the points to polygons and apply the color
-        // gradient
+    let width = $window_dims.window_width / 2.5;
+    let height = $dims.doc_height;
 
-        if ($dims.doc_height != 0) {
-            if (pattern_container.innerHTML != "") {
-                pattern_container.innerHTML = "";
-            }
+    var numCols = 20;
 
-            var width = $window_dims.window_width / 2;
+    let bounding_w = width / numCols;
+    var padding = 4;
+    var circle_r = bounding_w - padding;
 
-            var height = $dims.doc_height - $dims.footer_height;
-            
-            var pattern = trianglify({
-                height,
-                width,
-                cellSize: 80,
-                strokeWidth: 2,
-                fill: false,
-                yColors: ["#3273DC", "#F4F4F4"],
-            });
+    var numRows = Math.floor(height / bounding_w);
 
-            // use the toCanvas function to render the generated geometry to an HTML5
-            // canvas element
-            pattern.toSVG(pattern_container);
-        }
-        else{
+    // var numRowsRemainder = height / bounding_w - numRows;
 
-        }
+    var genColor = d3
+        .scaleSequential()
+        .domain([0, height])
+        .interpolator(d3.interpolateRainbow);
+
+    var rows = [];
+    var temp = padding;
+    for (let i = 0; i < numRows; i++) {
+        rows.push({ y: temp , c: genColor(temp) });
+        temp = temp + bounding_w;
     }
 
-    onMount(async () => {
-        draw_pattern();
-    });
+    var cols = [];
+    var temp = padding;
+    for (let i = 0; i < numCols; i++) {
+        cols.push({ x: temp});
+        temp = temp + bounding_w;
+    }
+
+    console.log(rows, cols)
 </script>
 
-<svelte:window on:resize={draw_pattern} />
+<svelte:window />
 
-<svg
-    fill="none"
-    height="100%"
-    width="50%"
+<div
     bind:this={pattern_container}
-    class="absolute top-0 hidden lg:block z-20"
-/>
+    class="absolute top-0 hidden lg:block z-20 opacity-75"
+>
+    <svg {width} {height}>
+            {#each cols as col}
+                <g>
+                    {#each rows as row}
+                        <rect
+                            x={col.x}
+                            y={row.y}
+                            fill={row.c}
+                            width={circle_r}
+                            height={circle_r}
+                        />
+                    {/each}
+                </g>
+            {/each}
+    </svg>
+</div>
 
 <style>
-    svg {
-        clip-path: polygon(
-            0 0,
-            40% 25%,
-            0 50%,
-            40% 75%,
-            0 100%,
-            40% 100%,
-            80% 75%,
-            40% 50%,
-            80% 25%,
-            40% 0
-        );
-    }
 </style>
