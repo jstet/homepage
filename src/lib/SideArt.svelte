8204dd89897181c636da93cafed7d0baecebfcd4
Jonas Stettner <mail@jstet.net>
2022-10-11 00:02:03 +0200

    finished new selfmade sideart

--- src/lib/SideArt.svelte
+++ src/lib/SideArt.svelte
@@ -1,66 +1,75 @@
 <script>
+    import { onMount } from "svelte";
     import { dims, window_dims } from "$lib/stores/dimensions.js";
     import * as d3 from "d3";
+    import _ from "lodash";
+    let cols = [];
+    let rows = [];
+    let box;
 
-    let pattern_container;
+    let height, width;
 
-    let width = $window_dims.window_width / 2.5;
-    let height = $dims.doc_height;
+    $: width = $window_dims.window_width / 2.5;
+    $: height = $dims.doc_height;
 
-    var numCols = 20;
+    const draw = (width, height) => {
+        cols = [];
+        rows = [];
+        const padding = 18;
+        const bounding_w = 50;
 
-    let bounding_w = width / numCols;
-    var padding = 4;
-    var circle_r = bounding_w - padding;
+        var numCols = (width - padding) / bounding_w;
 
-    var numRows = Math.floor(height / bounding_w);
+        box = bounding_w - padding;
 
-    // var numRowsRemainder = height / bounding_w - numRows;
+        var numRows = Math.floor(height / bounding_w);
 
-    var genColor = d3
-        .scaleSequential()
-        .domain([0, height])
-        .interpolator(d3.interpolateRainbow);
+        var genColor = d3
+            .scaleSequential()
+            .domain([0, height])
+            .interpolator(d3.interpolateRainbow);
 
-    var rows = [];
-    var temp = padding;
-    for (let i = 0; i < numRows; i++) {
-        rows.push({ y: temp , c: genColor(temp) });
-        temp = temp + bounding_w;
-    }
+        var temp = padding;
+        let st,
+            en = 0;
+        for (let i = 0; i < numRows; i++) {
+            st = _.random(0, numCols / 2 - 1);
+            en = _.random(numCols / 2 + 1, numCols);
+            rows.push({ y: temp, c: genColor(temp), start: st, end: en });
+            temp = temp + bounding_w;
+        }
 
-    var cols = [];
-    var temp = padding;
-    for (let i = 0; i < numCols; i++) {
-        cols.push({ x: temp});
-        temp = temp + bounding_w;
-    }
+        var temp = padding;
+        for (let i = 0; i < numCols; i++) {
+            cols.push({ x: temp });
+            temp = temp + bounding_w;
+        }
+    };
 
-    console.log(rows, cols)
+    $: draw(width, height);
 </script>
 
-<svelte:window />
-
-<div
-    bind:this={pattern_container}
-    class="absolute top-0 hidden lg:block z-20 opacity-75"
->
-    <svg {width} {height}>
-            {#each cols as col}
+{#if cols}
+    <div class="absolute top-0 hidden lg:block z-20 opacity-25">
+        <svg {width} {height}>
+            {#each rows as row}
                 <g>
-                    {#each rows as row}
+                    {#each _.slice(cols, [row.start], [row.end]) as col}
                         <rect
                             x={col.x}
                             y={row.y}
                             fill={row.c}
-                            width={circle_r}
-                            height={circle_r}
+                            stroke-width="1 "
+                            stroke="#171717"
+                            width={box}
+                            height={box}
                         />
                     {/each}
                 </g>
             {/each}
-    </svg>
-</div>
+        </svg>
+    </div>
+{/if}
 
 <style>
 </style>
